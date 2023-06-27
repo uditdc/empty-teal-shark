42b74d31e557e08b09f271f76ec642b874037d7f
Udit <udit@eskaytech.com>
2023-06-28 04:44:32 +0530

    Include blockless dynamic routes

---
+++ assembly/routes/sub.ts
@@ -0,0 +1,21 @@
+
+/**
+ * Return the given value -1
+ * 
+ * @param val 
+ * @returns 
+ */
+export function GET(val: i64): i64 {
+  return val - 1
+}
+
+
+/**
+ * Return the given value -2
+ * 
+ * @param val 
+ * @returns 
+ */
+export function POST(val: i64): i64 {
+  return val - 2
+}
---
+++ assembly/routes/timestamp.ts
@@ -0,0 +1,19 @@
+/**
+ * Return the current timestamp
+ * 
+ * @param val 
+ * @returns 
+ */
+export function GET(val: i64): i64 {
+  return Date.now()
+}
+
+/**
+ * Return the current timestamp
+ * 
+ * @param val 
+ * @returns 
+ */
+export function POST(val: i64): i64 {
+  return Date.now()
+}
